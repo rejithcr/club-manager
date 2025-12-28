4cfccdf634c45358d9ed473946c2e4da07bdfe81
Rejith <rejithramakrishnan@gmail.com>
2025-12-28 21:29:23 +0530

    commiting conts file after long

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,8 +1,6 @@
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://n24tjyszxraupd5cuogxf377i40xiltl.lambda-url.ap-south-1.on.aws';
+//export const BASE_URL = 'http://localhost:5000';
 
 export const ROLE_ADMIN="ADMIN"
 export const ROLE_MEMBER="MEMBER"
 
-export const PERIOD_MONTHLY = "MONTHLY"
-export const PERIOD_QUATERLY = "QUATERLY"
-export const PERIOD_YEARLY = "YEARLY"
